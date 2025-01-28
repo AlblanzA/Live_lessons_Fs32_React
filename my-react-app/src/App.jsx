282f1d1cf5e48ef660aac0512cda62209d37fd32
Alberto Lanzafame <lanzafamea2001@gmail.com>
2025-01-28 01:35:35 +0100

    update

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,72 +1,51 @@
 
-import Card from "./Components/Card"
+import { Card3, Card, Card2 } from './Components/Card'
 import './App.css'
+import { useEffect , useState } from 'react';
 
 function App() {
-  // const [count, setCount] = useState(0)
+  //primo valore è il nome della variabile, 
+  // il secondo valore è il metodo utilizzato per cambiare il valore della variabile
+   const [books, setBooks] = useState(null)
+   const baseUrl = "https://api.potterdb.com/"
+   
+   //useEffet è utilizzato per le azioni da fare quando il componente viene ad esempio montato
+   //e allora faccio la chiamata all'API - in generale viene usata per montare un componente
+   useEffect(() => {
+    console.log('I have mounted!');
+    fetchData()
+    }, []);
+
+   async function fetchData() {
+    const response = await fetch (baseUrl + "/v1/books")
+    console.log(response.status);
+    
+    if (response.status != 200) {
+      console.log("Error fetch code: " + response.status);
+    } else {
+       //estrapolo i dati ricevuti dal fetch in una variabile
+       const responseData = await response.json();
+       console.log(responseData.data);
+
+       //sto inserendo responeData.data dentro la variabile books di useState
+       setBooks(responseData.data)
+   }
+  }
+
+console.log(books);
+
 
   return (
     <>
+   <h1>Home</h1>
 
-    <Card></Card>
-      <div>
-      <a href="#" className="group relative block overflow-hidden">
-  <button
-    className="absolute end-4 top-4 z-10 rounded-full bg-white p-1.5 text-gray-900 transition hover:text-gray-900/75"
-  >
-    <span className="sr-only">Wishlist</span>
-
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
-      strokeWidth="1.5"
-      stroke="currentColor"
-      className="size-4"
-    >
-      <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
-      />
-    </svg>
-  </button>
-
-  <img
-    src="https://images.unsplash.com/photo-1599481238640-4c1288750d7a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2664&q=80"
-    alt=""
-    className="h-64 w-full object-cover transition duration-500 group-hover:scale-105 sm:h-72"
-  />
-
-  <div className="relative border border-gray-100 bg-white p-6">
-    <span className="whitespace-nowrap bg-yellow-400 px-3 py-1.5 text-xs font-medium"> New </span>
-
-    <h3 className="mt-4 text-lg font-medium text-gray-900">Robot Toy</h3>
-
-    <p className="mt-1.5 text-sm text-gray-700">$14.99</p>
-
-    <form className="mt-4">
-      <button
-        className="block w-full rounded bg-yellow-400 p-4 text-sm font-medium transition hover:scale-105"
-      >
-        Add to Cart
-      </button>
-    </form>
-  </div>
-</a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-       
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <Card/>
+    <Card2/>
+    <Card3/>
+   
     </>
   )
 }
+
 
 export default App
--- my-react-app/src/Components/Card.jsx
+++ my-react-app/src/Components/Card.jsx
@@ -1,8 +1,35 @@
-function Card(){
+import { useEffect } from "react";
+
+export function Card(){
+  useEffect(() => {
+    console.log('I have mounted Card!');
+    }, []);
+
+   return( 
+  <>
+ <h1 className="">Ciao</h1>
+  </>
+   )
+
+   
+}
+
+export function Card2(){
    return( 
   <>
  <h1>Ciao</h1>
   </>
    );
 }
-export default Card();
+
+export function Card3(){
+   return( 
+  <>
+ <h1>Ciao</h1>
+  </>
+   );
+}
+
+//uso default se ho soltanto 1 componente
+// export default Card();
+
