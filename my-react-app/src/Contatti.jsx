a357d487eee726834d6c49a32cf1fa9322bf66b6
natasciarufino <natasciarufino17@gmail.com>
2025-02-04 10:01:27 +0100

    update

---
+++ my-react-app/src/Contatti.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Navbar from "./Components/Navbar";
+import Footer from "./Components/Footer";
+
+function Contatti(){
+    let logoFooter = 'https://www.freeiconspng.com/thumbs/harry-potter-logo/harry-potter-logo-png-19.png'
+    return(
+         <>
+        <Navbar logo={logoFooter}/> 
+        <h1>Contatti</h1>
+        <Footer logo={logoFooter} />
+        </>
+    )
+    
+}
+
+export default Contatti;
--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -1,11 +1,31 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
-import { BrowserRouter } from "react-router";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom"
+import App from './App';
+import Contatti from './Contatti';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>
+  },
+  {
+    path: "about",
+    element: <div>About</div>,
+  },
+  {
+    path: "contatti",
+   element: <Contatti/>,
+  },
+]);
 
 // lo <StrictMode> lo utilizziamo per la fase di sviluppo e di test per mostrare gli errori
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-  <App />
-</BrowserRouter>
+    <RouterProvider router={router} />
+ 
 )
