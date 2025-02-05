6e29799a024e8a491f59300fb0f5109627c8ac99
marienne86 <mariaconcettapuzzo@gmail.com>
2025-02-05 11:12:02 +0100

    fetch star wars films

--- my-react-app/src/Pages/StarWars.jsx
+++ my-react-app/src/Pages/StarWars.jsx
@@ -2,10 +2,27 @@ import React from "react";
 import Navbar from "../Components/Navbar";
 import Footer from "../Components/Footer";
 import Header from "../Components/Header";
+import {useState} from "react";
 
 export default function StarWars() {
   let logo =
     "https://loodibee.com/wp-content/uploads/Star-Wars-transparent-logo.png";
+    const [films, setFilms]= useState([])
+    const baseUrl="https://swapi.dev/api/"
+
+    async function fetchData(){
+      const response= await fetch (baseUrl +"films")
+    
+
+      if (response.status !=200){
+     console.log ("fetchData Error")
+    }else{
+      const responseData= await response.json();
+      console.log (responseData.data)
+    }
+    }
+  fetchData()
+
   return (
     <>
       <Navbar logo={logo} />
@@ -14,3 +31,4 @@ export default function StarWars() {
     </>
   );
 }
+
