8778163d7d8ec49d4aa03834bd8aec250b23fbb7
marienne86 <mariaconcettapuzzo@gmail.com>
2025-02-06 10:26:35 +0100

    starwars fetch

--- my-react-app/src/Pages/StarWars.jsx
+++ my-react-app/src/Pages/StarWars.jsx
@@ -1,32 +1,49 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Navbar from "../Components/Navbar";
 import Footer from "../Components/Footer";
 import Header from "../Components/Header";
-import {useState} from "react";
+import { useState } from "react";
 
 export default function StarWars() {
   let logo =
     "https://loodibee.com/wp-content/uploads/Star-Wars-transparent-logo.png";
-    const [films, setFilms]= useState([])
-    const baseUrl="https://swapi.dev/api/"
+  const [films, setFilms] = useState([]);
+  const baseUrl = "https://swapi.dev/api/";
 
-    async function fetchData(){
-      const response= await fetch (baseUrl +"films")
-    
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-      if (response.status !=200){
-     console.log ("fetchData Error")
-    }else{
-      const responseData= await response.json();
-      console.log (responseData.data)
+  async function fetchData() {
+    const response = await fetch(baseUrl + "films");
+
+    if (response.status != 200) {
+      console.log("fetchData Error");
+    } else {
+      const responseData = await response.json();
+      //salvo i film nella variabile films
+      setFilms(responseData.results);
+      console.log(films);
     }
-    }
-  fetchData()
+  }
 
   return (
     <>
       <Navbar logo={logo} />
       <Header title={"StarWars"} />
+
+      <div classname = "filmsContainer">
+    {films.lenght =!0 &&
+    films.map((film)=>(
+      <h1>{film.title} </h1>
+
+
+    ))}
+
+      </div>
+
+      
+
       <Footer logo={logo} />
     </>
   );
