37d653708600799654d6d479e89e6c7205f5a7ec
Alberto Lanzafame <lanzafamea2001@gmail.com>
2025-02-13 10:57:59 +0100

    update

--- my-react-app/src/Pages/Login.jsx
+++ my-react-app/src/Pages/Login.jsx
@@ -1,117 +1,161 @@
+import { useState } from "react";
 
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-export default function Login(){
-    return(
-<>
-{/*
-  Heads up! 👋
+  //creo una costante con una funzione per attivare l'evento Onchange nell'input html alla riga 41
+  const handleChange = (e) => {
+    //stampo tutto l'elemento html che ascolto usando onChange
+    // console.log(e.target);
 
-  Plugins:
-    - @tailwindcss/forms
-*/}
+    //stampo solo l'attributo name dell'elemento che sto acoltando usando onChange
+    // console.log(e.target.name);
 
-<section className="relative flex flex-wrap lg:h-screen lg:items-center">
-  <div className="w-full px-4 py-12 sm:px-6 sm:py-16 lg:w-1/2 lg:px-8 lg:py-24">
-    <div className="mx-auto max-w-lg text-center">
-      <h1 className="text-2xl font-bold sm:text-3xl">Get started today!</h1>
+    //stampo il value che ha inserito l'utente nell'elemento che sto ascoltando usando onChange
+    // console.log(e.target.value)
 
-      <p className="mt-4 text-gray-500">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Et libero nulla eaque error neque
-        ipsa culpa autem, at itaque nostrum!
-      </p>
-    </div>
+    if (e.target.name == "email") {
+      if (e.target.value == "{") {
+        alert("Hai inserito un carattere non valido");
+      }
+      setEmail(e.target.value);
+      console.log("hai scritto la mail");
+      console.log(email);
+    }
+    if (e.target.name == "password") {
+      setPassword(e.target.value);
+      console.log("hai scritto la password");
+      console.log(password);
+    }
+    //target è associato al puntatore
+  };
 
-    <form action="#" className="mx-auto mt-8 mb-0 max-w-md space-y-4">
-      <div>
-        <label htmlFor="email" className="sr-only">Email</label>
+  //funzione per la Login che connettiamo all'attributo onSubmit alla riga 55 , ovvero quando il form viene inviato
+  function handleSubmit(e) {
+    alert("Login effettuato");
+  }
 
-        <div className="relative">
-          <input
-            type="email"
-            className="w-full rounded-lg border-gray-200 p-4 pe-12 text-sm shadow-xs"
-            placeholder="Enter email"
-          />
+  return (
+    <>
+      <section className="relative flex flex-wrap lg:h-screen lg:items-center">
+        <div className="w-full px-4 py-12 sm:px-6 sm:py-16 lg:w-1/2 lg:px-8 lg:py-24">
+          <div className="mx-auto max-w-lg text-center">
+            <h1 className="text-2xl font-bold sm:text-3xl">
+              Get started today!
+            </h1>
 
-          <span className="absolute inset-y-0 end-0 grid place-content-center px-4">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="size-4 text-gray-400"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207"
-              />
-            </svg>
-          </span>
+            <p className="mt-4 text-gray-500">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Et libero
+              nulla eaque error neque ipsa culpa autem, at itaque nostrum!
+            </p>
+          </div>
+
+          <form
+            onSubmit={handleSubmit}
+            className="mx-auto mt-8 mb-0 max-w-md space-y-4"
+          >
+            <div>
+              <label htmlFor="email" className="sr-only">
+                Email
+              </label>
+
+              <div className="relative">
+                <input
+                  type="email"
+                  className="w-full rounded-lg border-gray-200 p-4 pe-12 text-sm shadow-xs"
+                  placeholder="Enter email"
+                  value={email}
+                  name="email"
+                  //onchage è l'ascolto di un evento , in questo caso la funzione handleChange
+                  onChange={handleChange}
+                />
+
+                <span className="absolute inset-y-0 end-0 grid place-content-center px-4">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="size-4 text-gray-400"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207"
+                    />
+                  </svg>
+                </span>
+              </div>
+            </div>
+
+            <div>
+              <label htmlFor="password" className="sr-only">
+                Password
+              </label>
+
+              <div className="relative">
+                <input
+                  type="password"
+                  className="w-full rounded-lg border-gray-200 p-4 pe-12 text-sm shadow-xs"
+                  placeholder="Enter password"
+                  value={password}
+                  name="password"
+                  onChange={handleChange}
+                />
+
+                <span className="absolute inset-y-0 end-0 grid place-content-center px-4">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="size-4 text-gray-400"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                    />
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
+                    />
+                  </svg>
+                </span>
+              </div>
+            </div>
+
+            <div className="flex items-center justify-between">
+              <p className="text-sm text-gray-500">
+                No account?
+                <a className="underline" href="#">
+                  Sign up
+                </a>
+              </p>
+
+              <button
+                type="submit"
+                className="inline-block rounded-lg bg-blue-500 px-5 py-3 text-sm font-medium text-white"
+              >
+                Sign in
+              </button>
+            </div>
+          </form>
         </div>
-      </div>
 
-      <div>
-        <label htmlFor="password" className="sr-only">Password</label>
-
-        <div className="relative">
-          <input
-            type="password"
-            className="w-full rounded-lg border-gray-200 p-4 pe-12 text-sm shadow-xs"
-            placeholder="Enter password"
+        <div className="relative h-64 w-full sm:h-96 lg:h-full lg:w-1/2">
+          <img
+            alt=""
+            src="https://www.lyoncapitale.fr/wp-content/uploads/2014/11/513405-ez-Star-Wars-936x585.jpg"
+            className="absolute inset-0 h-full w-full object-cover"
           />
-
-          <span className="absolute inset-y-0 end-0 grid place-content-center px-4">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="size-4 text-gray-400"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-              />
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
-              />
-            </svg>
-          </span>
         </div>
-      </div>
-
-      <div className="flex items-center justify-between">
-        <p className="text-sm text-gray-500">
-          No account?
-          <a className="underline" href="#">Sign up</a>
-        </p>
-
-        <button
-          type="submit"
-          className="inline-block rounded-lg bg-blue-500 px-5 py-3 text-sm font-medium text-white"
-        >
-          Sign in
-        </button>
-      </div>
-    </form>
-  </div>
-
-  <div className="relative h-64 w-full sm:h-96 lg:h-full lg:w-1/2">
-    <img
-      alt=""
-      src="https://www.lyoncapitale.fr/wp-content/uploads/2014/11/513405-ez-Star-Wars-936x585.jpg"
-      className="absolute inset-0 h-full w-full object-cover"
-    />
-  </div>
-</section>
-
-</>
-
-    )
+      </section>
+    </>
+  );
 }
